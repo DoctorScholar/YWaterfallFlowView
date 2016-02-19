88875cc087c48b93b0316cf41f4c234bcd83aef2
IOS_Doctor <1764106613@qq.com>
2016-02-19 10:50:35 +0800

    modify version 1.0.2

--- YWaterfallFlowView/JZWaterfallFlowViewController.h
+++ YWaterfallFlowView/JZWaterfallFlowViewController.h
@@ -81,3 +81,35 @@ NS_CLASS_AVAILABLE_IOS(2_0) @interface JZWaterfallFlowViewController : UIScrollV
 - (void)layoutSubviewsW;
 
 @end
+
+/**
+ UIView category relevant with frame
+ */
+@interface UIView (frame)
+
+//sets frame.origin.x = left;
+@property (nonatomic) CGFloat left;
+//sets frame.origin.y = top;
+@property (nonatomic) CGFloat top;
+//sets frame.origin.x = right - frame.size.wigth;
+@property (nonatomic) CGFloat right;
+//sets frame.origin.y = botton - frmae.size.height;
+@property (nonatomic) CGFloat bottom;
+//sets frame.size.width = width;
+@property (nonatomic) CGFloat width;
+//sets frame.size.height = height;
+@property (nonatomic) CGFloat height;
+//sets center.x = centerX;
+@property (nonatomic) CGFloat centerX;
+//sets center.y = centerY;
+@property (nonatomic) CGFloat centerY;
+//frame.origin
+@property (nonatomic) CGPoint origin;
+//frame.size
+@property (nonatomic) CGSize size;
+
+- (void)removeAllSubviews;
+
+- (void)addSubviews:(NSArray *)subviews;
+
+@end
